8db05ce57d1a7eb7199216766fe8add4496e7348
mpish <nachmadmaruf@gmail.com>
2026-06-09 03:48:45 +0700

    fix:pindah urutan css di app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,13 +12,15 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="{{ asset('css/theme-mode.css') }}">
 
+ <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        
         <!-- Admin Custom Styles -->
 
         <link rel="stylesheet" href="{{ asset('css/smk-style.css') }}">
         <link rel="stylesheet" href="{{ asset('css/forms-style.css') }}">
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+       
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
